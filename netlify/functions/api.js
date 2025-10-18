65e95c830c4746010046c4fa176b9e565c718802
serenmaze <341019948@qq.com>
2025-10-18 18:50:07 +0800

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const serverless = require('@netlify/express');
+ 
 const app = express();
  
-// 这里是你的Express应用代码
-require('../../index.js');
+// 静态文件
+app。use(express.static('assets'));
+ 
+// 主页路由
+app.get('/', (req, res) => {
+  res.send('Moe Counter is running!');
+});
+ 
+// 计数器路由（简化版）
+app.get('/@:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Counter for ${name}: ${Math.floor(Math.random() * 1000)}`);
+});
  
 module.exports = serverless(app);
